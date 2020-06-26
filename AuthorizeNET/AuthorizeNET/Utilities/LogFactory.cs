6a1b560cc248f7f05f1205b143cb3f1f775319ed
Furkan Duman <furkan0duman@gmail.com>
2020-06-26 13:54:08 +0300

    Bump dotnet core 3.1

--- AuthorizeNET/AuthorizeNET/Utilities/LogFactory.cs
+++ AuthorizeNET/AuthorizeNET/Utilities/LogFactory.cs
@@ -1,11 +1,18 @@
-﻿namespace AuthorizeNet.Utilities
+﻿using Microsoft.Extensions.Logging.Debug;
+
+namespace AuthorizeNet.Utilities
 {
     using System;
     using Microsoft.Extensions.Logging;
 
     public static class LogFactory
     {
-        private static ILoggerFactory LoggerFactory => new LoggerFactory().AddDebug(LogLevel.Debug);
+        static LogFactory()
+        {
+            LoggerFactory.AddProvider(new DebugLoggerProvider());
+        }
+        
+        private static ILoggerFactory LoggerFactory => new LoggerFactory();
 
         public static ILogger getLog(Type classType)
         {
